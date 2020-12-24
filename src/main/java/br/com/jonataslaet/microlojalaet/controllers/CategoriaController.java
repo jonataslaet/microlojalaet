ec5f06296005883c1e8e3bf5d6f01f46bf7f6343
Jonatas Laet <jonataslaetprogramador@gmail.com>
2020-12-24 00:01:29 -0300

    Criado o endpoint /categorias foi criado com duas instâncias de categoria, apenas para testar

--- src/main/java/br/com/jonataslaet/microlojalaet/controllers/CategoriaController.java
+++ src/main/java/br/com/jonataslaet/microlojalaet/controllers/CategoriaController.java
@@ -1,14 +1,28 @@
 package br.com.jonataslaet.microlojalaet.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.jonataslaet.microlojalaet.domain.Categoria;
+
 @RestController
+@RequestMapping("/categorias")
 public class CategoriaController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	String listar() {
-		return "Criado o primeiro endpoint REST";
+	List<Categoria> listar() {
+		List<Categoria> categorias = new ArrayList<>();
+
+		Categoria cat1 = new Categoria(1, "Escritório");
+		Categoria cat2 = new Categoria(2, "Informática");
+
+		categorias.add(cat1);
+		categorias.add(cat2);
+
+		return categorias;
 	}
 }
