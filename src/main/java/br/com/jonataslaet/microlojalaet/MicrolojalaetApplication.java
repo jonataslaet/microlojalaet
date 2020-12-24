806e5890f67524d6b7bfa658058aa1335968d46a
Jonatas Laet <jonataslaetprogramador@gmail.com>
2020-12-24 10:55:39 -0300

    Criada operação de instanciação para Categoria foi adicionada na inicialização da aplicação a instanciação de 2 categorias e salvas no banco

--- src/main/java/br/com/jonataslaet/microlojalaet/MicrolojalaetApplication.java
+++ src/main/java/br/com/jonataslaet/microlojalaet/MicrolojalaetApplication.java
@@ -1,13 +1,30 @@
 package br.com.jonataslaet.microlojalaet;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MicrolojalaetApplication {
+import br.com.jonataslaet.microlojalaet.domain.Categoria;
+import br.com.jonataslaet.microlojalaet.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class MicrolojalaetApplication implements CommandLineRunner{
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MicrolojalaetApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Eletrônico");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
