900478fd9f7161f8edaf32123060303f1fee10da
Jonatas Laet <jonataslaetprogramador@gmail.com>
2020-12-24 11:43:29 -0300

    Criado o produto e a associação de Produto com Categoria ainda não foi criado endpoint para produto

--- src/main/java/br/com/jonataslaet/microlojalaet/MicrolojalaetApplication.java
+++ src/main/java/br/com/jonataslaet/microlojalaet/MicrolojalaetApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.jonataslaet.microlojalaet.domain.Categoria;
+import br.com.jonataslaet.microlojalaet.domain.Produto;
 import br.com.jonataslaet.microlojalaet.repositories.CategoriaRepository;
+import br.com.jonataslaet.microlojalaet.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class MicrolojalaetApplication implements CommandLineRunner{
 
 	@Autowired
 	CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MicrolojalaetApplication.class, args);
@@ -24,7 +29,20 @@ public class MicrolojalaetApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Eletrônico");
+		
+		Produto p1 = new Produto(null, "Notebook", 2780.00);
+		Produto p2 = new Produto(null, "Impressora", 790.00);
+		Produto p3 = new Produto(null, "Cadeira", 320.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().add(p2);
+		
+		p1.getCategorias().add(cat1);
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().add(cat1);
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
