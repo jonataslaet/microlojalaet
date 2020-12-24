9af330ea8fb308841fcaaff3d4daac65a8b224b0
Jonatas Laet <jonataslaetprogramador@gmail.com>
2020-12-23 23:29:34 -0300

    Projeto criado foi criado e executado com sucesso

---
+++ src/main/java/br/com/jonataslaet/microlojalaet/MicrolojalaetApplication.java
@@ -0,0 +1,13 @@
+package br.com.jonataslaet.microlojalaet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrolojalaetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrolojalaetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jonataslaet/microlojalaet/MicrolojalaetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jonataslaet.microlojalaet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrolojalaetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
