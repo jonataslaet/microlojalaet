c291fee386a6308cb63261de6b30917b99de353a
Jonatas Laet <jonataslaetprogramador@gmail.com>
2020-12-26 12:24:11 -0300

    Adicionados os modelos de Estado e Cidade foram feitas também as devidas associações entre os dois modelos foram adicionadas algumas instâncias dos modelos, apenas para testar

---
+++ src/main/java/br/com/jonataslaet/microlojalaet/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package br.com.jonataslaet.microlojalaet.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.jonataslaet.microlojalaet.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
---
+++ src/main/java/br/com/jonataslaet/microlojalaet/services/EstadoService.java
@@ -0,0 +1,8 @@
+package br.com.jonataslaet.microlojalaet.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EstadoService {
+
+}
