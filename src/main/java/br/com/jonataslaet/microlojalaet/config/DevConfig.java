5bbe8f3d77b6d3ccfe8915d9c9fd2ca448a203d8
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-01-02 06:16:11 -0300

    Criado o perfil de dev, e criado o banco de dados mysql foi também feito um ajuste para instanciar banco apenas se a estrategía for create

---
+++ src/main/java/br/com/jonataslaet/microlojalaet/config/DevConfig.java
@@ -0,0 +1,32 @@
+package br.com.jonataslaet.microlojalaet.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.jonataslaet.microlojalaet.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	String strategy;
+
+	@Bean
+	public boolean instanciaDatabase() throws ParseException {
+		
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
+		return dbService.instanciaDataBase();
+	}
+}
