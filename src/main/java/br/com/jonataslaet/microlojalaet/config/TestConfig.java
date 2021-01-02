dfc3010cad81208da52c02f28854150923a37aad
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-01-02 05:09:20 -0300

    Criado perfil test de configuração as instanciações testes do database foram movidas para o modelo DBService

---
+++ src/main/java/br/com/jonataslaet/microlojalaet/config/TestConfig.java
@@ -0,0 +1,23 @@
+package br.com.jonataslaet.microlojalaet.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.jonataslaet.microlojalaet.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	DBService dbService;
+
+	@Bean
+	public boolean instanciaDatabase() throws ParseException {
+		return dbService.instanciaDataBase();
+	}
+}
