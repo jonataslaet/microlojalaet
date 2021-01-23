24a1dbe34a11ff8d435716dd811d7dc6036a1b84
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-01-23 15:44:16 -0300

    Implementada a autorização outras melhorias relacionadas à autorização ainda serão implementadas

--- src/main/java/br/com/jonataslaet/microlojalaet/config/SecurityConfig.java
+++ src/main/java/br/com/jonataslaet/microlojalaet/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.com.jonataslaet.microlojalaet.security.JWTAuthenticationFilter;
+import br.com.jonataslaet.microlojalaet.security.JWTAuthorizationFilter;
 import br.com.jonataslaet.microlojalaet.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
