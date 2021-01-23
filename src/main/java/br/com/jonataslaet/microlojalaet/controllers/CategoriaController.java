e9e1c747bb950e1d8b5e278d18286efd31197f13
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-01-23 16:08:36 -0300

    Implementada autorização para perfis específicos foi modificado também otempo de expiração, apenas para testes em desenvolvimento

--- src/main/java/br/com/jonataslaet/microlojalaet/controllers/CategoriaController.java
+++ src/main/java/br/com/jonataslaet/microlojalaet/controllers/CategoriaController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,16 +48,19 @@ public class CategoriaController {
 		return ResponseEntity.ok().body(categoria);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method = RequestMethod.POST)
 	ResponseEntity<?> insert(@Valid @RequestBody CategoriaDTO categoria) {
 		return cs.insert(categoria);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
 	ResponseEntity<?> update(@Valid @RequestBody CategoriaDTO categoria, @PathVariable Integer id) {
 		return cs.update(categoria, id);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}", method = RequestMethod.DELETE)
 	ResponseEntity<?> delete(@PathVariable Integer id) {
 		return cs.delete(id);
