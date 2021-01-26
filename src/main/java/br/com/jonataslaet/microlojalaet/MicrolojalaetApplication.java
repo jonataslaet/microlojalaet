176e292cdbfc16f33e325147fd3dc5c8068a6845
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-01-25 23:36:35 -0300

    Implementado primeiro upload no S3 Amazon foram removidos: senha do gmail, access key e secret key do application properties

--- src/main/java/br/com/jonataslaet/microlojalaet/MicrolojalaetApplication.java
+++ src/main/java/br/com/jonataslaet/microlojalaet/MicrolojalaetApplication.java
@@ -1,19 +1,25 @@
 package br.com.jonataslaet.microlojalaet;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.jonataslaet.microlojalaet.services.S3Service;
+
 @SpringBootApplication
 public class MicrolojalaetApplication implements CommandLineRunner{
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MicrolojalaetApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("/home/maida/Imagens/eumesmonoaviao.jpeg");
 	}
 
 }
